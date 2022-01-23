1f3632a3bf68701f7b92a89bfe4bef67732035df
jacken <jackenliy@gmail.com>
2022-01-23 22:05:47 +0800

    feat: update answer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center" style="font-size:25px;">Typescript Type Challenge</p>
+
+<P align="center">This is a Typesciprt Type Challenge for <a href="https://github.com/type-challenges/type-challenges">Type Challenge</a></p>
---
+++ src/CamelCase.ts
@@ -0,0 +1,47 @@
+/*
+  610 - CamelCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+  
+  ### Question
+  
+  `for-bar-baz` -> `forBarBaz`
+  
+  > View on GitHub: https://tsch.js.org/610
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type UpFirst<S extends string> = S extends `${infer F}${infer Rest}`
+  ? `${Uppercase<F>}${Rest}`
+  : S
+type CamelCase<S extends string> = S extends `${infer F}-${infer Rest}`
+  ? Rest extends UpFirst<Rest>
+    ? `${F}-${CamelCase<Rest>}`
+    : `${F}${CamelCase<UpFirst<Rest>>}`
+  : S
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CamelCase<'foo-bar-baz'>, 'fooBarBaz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-Baz'>, 'foo-Bar-Baz'>>,
+  Expect<Equal<CamelCase<'foo-Bar-baz'>, 'foo-BarBaz'>>,
+  Expect<Equal<CamelCase<'foo-bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<CamelCase<'foo--bar----baz'>, 'foo-Bar---Baz'>>,
+  Expect<Equal<CamelCase<'a-b-c'>, 'aBC'>>,
+  Expect<Equal<CamelCase<'a-b-c-'>, 'aBC-'>>,
+  Expect<Equal<CamelCase<'ABC'>, 'ABC'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/610/answer
+  > View solutions: https://tsch.js.org/610/solutions
+  > More Challenges: https://tsch.js.org
+*/
